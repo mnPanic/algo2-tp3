8cd88bf294e686a043232604f92df8a8ab63c5f6
grosinov <grosinov@gmail.com>
2019-06-26 19:20:23 -0300

    Juego: paso las funciones al cpp para implementarlas

--- Solucion/src/modelo/ExtremeExorcism.cpp
+++ Solucion/src/modelo/ExtremeExorcism.cpp
@@ -1,3 +1,28 @@
 #include "ExtremeExorcism.h"
 
-// Completar
+ExtremeExorcism::ExtremeExorcism(Habitacion h, set<Jugador> jugadores, PosYDir f_init, list<Accion> acciones_fantasma,
+        Contexto *ctx) {}
+
+void ExtremeExorcism::pasar() {}
+
+void ExtremeExorcism::ejecutarAccion(Jugador j, Accion a) {}
+
+list<pair<Jugador, PosYDir>> ExtremeExorcism::posicionJugadores() const {}
+
+list<PosYDir> ExtremeExorcism::posicionFantasmas() const {}
+
+PosYDir ExtremeExorcism::posicionEspecial() const {}
+
+list<PosYDir> ExtremeExorcism::disparosFantasmas() const {}
+
+set<Pos> ExtremeExorcism::posicionesDisparadas() const {}
+
+bool ExtremeExorcism::jugadorVivo(Jugador j) const {}
+
+const Habitacion& ExtremeExorcism::habitacion() const {}
+
+PosYDir ExtremeExorcism::posicionJugador(Jugador j) const {}
+
+const set<Jugador>& ExtremeExorcism::jugadores() const {}
+
+const list<Fantasma>& ExtremeExorcism::fantasmas() const {}
